193535598b4f29037473331c9ddeb45be8a7f029
Luiz Eduardo Pantaleão <134229613+LuizEduPantaleao@users.noreply.github.com>
2023-07-19 22:09:59 -0300

    Update index.java

--- index.java
+++ index.java
@@ -4,6 +4,9 @@ public class index
     {
         new GUI();
         System.out.println("Hello, World!");
+        
+        
+        
         /*
         Editora a = new Editora("a", "aaaa");
         Revista umaRevista = new Revista("Hey", "Nada", "p1", a, "mensalmente");
@@ -16,5 +19,10 @@ public class index
         Revista b = (Revista) Publicacao.importarPub("a.bin");
         System.out.println(b.editora.numPublicacoes);
         */
+
+        // Parte nova (Questão F)
+        Editora c = new Editora("HTML5 Pocket", "http://shop.oreilly.com/0636920029274.do");
+        Publicacao ca = new Publicacao ("HTML5 Pocket", "Estudos HTML5", "5th Edition", c, "Jennifer Robbis");
+        System.out.println(Editora.numPublicacoes);
     }
 }
